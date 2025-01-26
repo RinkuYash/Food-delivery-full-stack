1fd1acaa16a7692fe4ee179552fde243791dbfcc
Yash Dhiman <dhimanyash886@gmail.com>
2025-01-26 18:10:09 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://rinkuyash:Yashdhiman123@cluster0.opcie.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://'DBname':'Password'@cluster0.opcie.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
 
